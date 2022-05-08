ac81883ee0b8a4d406d870992afed1f4ef40f6de
Vsevolod Poletaev <poletaev.va@gmail.com>
2022-05-08 15:37:40 +0400

    :bug: Remove type validation from dict_merge Type validation is performed by pydantic

--- doh/config.py
+++ doh/config.py
@@ -30,10 +30,6 @@ def dict_merge(*args, add_keys=True):
         for k, v in merge_dct.items():
             if not rtn_dct.get(k):
                 rtn_dct[k] = v
-            elif k in rtn_dct and type(v) != type(rtn_dct[k]):
-                raise TypeError(
-                    f"Overlapping keys exist with different types: original is {type(rtn_dct[k])}, new value is {type(v)}"
-                )
             elif isinstance(rtn_dct[k], dict) and isinstance(
                 merge_dct[k], collections.abc.Mapping
             ):
